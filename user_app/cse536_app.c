9f6558e80e512c8148b899bb18a7841056644251
shashank karkare <karkareshashank@gmail.com>
2014-11-08 16:17:20 -0800

    working on testing app for the module

---
+++ user_app/cse536_app.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <error.h>
+
+#define DEVICE_NAME	"/dev/cse536"
+#define MSG_SIZE	256
+#define IPPROTO_CSE536	234
+
+
+int main(int argc, char **argv)
+{
+	int fd;
+
+	
+
+
+
+
+
+
+	return 0;
+}
