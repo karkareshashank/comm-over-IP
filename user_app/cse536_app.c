e9d710821a9f62d77bdfdc6359f478caf0950a10
shashank karkare <karkareshashank@gmail.com>
2014-11-11 08:06:09 -0800

    Test app is not more user friendly and easily accessible.

--- user_app/cse536_app.c
+++ user_app/cse536_app.c
@@ -9,11 +9,11 @@
 #include <sys/types.h>
 
 #define DEVICE_NAME	"/dev/cse536"
-#define TEST_MSG	"Assassin's Creed: Unity from Machine 2"
-#define DEST_ADDR	"192.168.136.130"
 #define MAX_MSG_SIZE	256
-#define IPPROTO_CSE536	234
+#define ADDRESS_LEN	16
 
+
+// Sets the destination address in teh character device
 int  set_daddr(int fd, char *addr)
 {
 	int ret = 0;
@@ -31,12 +31,13 @@ int  set_daddr(int fd, char *addr)
 }
 
 
+// Sends the message to  the character device
 int send_msg(int fd, char *msg)
 {
 	int ret = 0;
 	char *data = (char *)malloc(sizeof(char) * (strlen(msg) + 1));
 
-	data[0] = '0';
+	data[0] = '2';
 	strcpy(data+1, msg);
 
 	ret = write(fd, (void *)data, strlen(data));
@@ -47,6 +48,8 @@ int send_msg(int fd, char *msg)
 	return ret > 0 ? 1 : -1;
 }
 
+
+// Gets the message from teh buffer in the device
 int recv_msg(int fd, char *msg)
 {
 	int ret = 0;
@@ -56,11 +59,28 @@ int recv_msg(int fd, char *msg)
 	return ret >= 0 ? 1 : -1;
 }
 
+
+// Prints choices for the user
+void print_choices()
+{
+	printf("------------------------------\n");
+	printf("1 --- Set destination address \n");
+	printf("2 --- Send message	      \n");
+	printf("3 --- Read Message	      \n");
+	printf("4 --- Exit		      \n");
+	printf("------------------------------\n");
+	printf("Enter your choice: ");
+}
+
+
+// Main code
 int main(int argc, char **argv)
 {
 	int fd;
+	char fix;
 	int ret = 0;
 	char *data = NULL;
+	int choice;
 
 	// Allocate memory for buffer
 	data = (char *)malloc(sizeof(char)* MAX_MSG_SIZE);
@@ -78,36 +98,60 @@ int main(int argc, char **argv)
 		goto free_data;
 	}
 
-	// Set the address
-	if (set_daddr(fd, DEST_ADDR) == -1) {
-		printf("%s: Error setting the address\n", __FILE__);
-		ret = -1;
-		goto close_file;
-	}	
 
-	// Send the data
-	strcpy(data, TEST_MSG);
-	printf("%s: Sedning message = %s\n",__FILE__, data);
-	if (send_msg(fd, data) == -1) {
-		printf("%s: Error sending the message\n",__FILE__);
-		ret = -1;
-		goto close_file;
+	while(1) {
+
+		print_choices();
+		scanf("%d%c",&choice,&fix);
+		fflush(stdin);
+
+		switch(choice) {
+
+			case 1:
+				// Set the address
+				printf("Enter the address: ");
+				fgets(data, ADDRESS_LEN,stdin);
+				if (set_daddr(fd, data) == -1) {
+					printf("%s: Error setting the address\n", __FILE__);
+					ret = -1;
+					goto close_file;
+				}	
+				printf("Address set to %s successfully\n", data);
+				break;
+
+			case 2:
+				// Send the data
+				printf("Enter the message: ");
+				fgets(data, MAX_MSG_SIZE, stdin);
+				printf("%s: Sending message = %s\n",__FILE__, data);
+				if (send_msg(fd, data) == -1) {
+					printf("%s: Error sending the message\n",__FILE__);
+					ret = -1;
+					goto close_file;
+				}
+				break;
+
+			case 3:
+				// Read the data
+				memset(data, 0, MAX_MSG_SIZE);
+				if (recv_msg(fd, data) == -1) {
+					printf("%s: Error receiving message\n",__FILE__);
+					ret = -1;
+					goto close_file;
+				}
+				printf("%s: Message received = %s\n",__FILE__, data);
+				break;
+
+			case 4:
+				ret = 0;
+				goto close_file;
+			default:
+				printf("Invalid choice\n");
+		}
+
 	}
 
-
-	sleep(2);
-
-	// Read the data
-	memset(data, 0, MAX_MSG_SIZE);
-	if (recv_msg(fd, data) == -1) {
-		printf("%s: Error receiving message\n",__FILE__);
-		ret = -1;
-		goto close_file;
-	}
-	printf("%s: Message received = %s\n",__FILE__, data);
-
-
-
+		
 close_file:
 	close (fd);
 free_data:
